9a00cd5221a401ea1abfd34f0302a5f0b5e0d509
isayakmondal <isayakmondal@gmail.com>
2022-08-14 22:00:08 +0530

    add location[currently under dev]

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -3,14 +3,41 @@ const route = express.Router();
 const { notAuthorize } = require("../../functions/authFunc");
 const passport = require("passport");
 route.get("/", notAuthorize, (req, res) => {
-  res.render("auth/login.ejs", { tabName: "Login S-Meet" });
+  res.render("auth/login.ejs", { tabName: "Login Zooper" });
 });
-route.post(
-  "/",
+
+function getLocation() {
+
+  const success = (position)=>{
+      console.log(position);
+
+  }
+  const error = () =>{
+      console.log("Unable to get the location");
+  }
+
+  global.navigator.geolocation.getCurrentPosition(success, error);
+  
+}
+
+
+route.post("/",(req,res)=>{
+  getLocation();
   passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+        successRedirect: "/",
+        failureRedirect: "/login",
+        failureFlash: true,
+      })
+  // res.redirect("/");
+});
+
+  // route.post(
+  //   "/",
+  //   passport.authenticate("local", {
+  //     successRedirect: "/",
+  //     failureRedirect: "/login",
+  //     failureFlash: true,
+  //   })
+  // );
 module.exports = route;
+
